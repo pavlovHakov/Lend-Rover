814b493d0a2105bdcf973b2432565d0a186cbb4f
oleg <278381@gmail.com>
2024-05-03 17:51:35 +0300

    работа с Vehicles

--- src/pages/vehicles/Vehicles.jsx
+++ src/pages/vehicles/Vehicles.jsx
@@ -1,3 +1,5 @@
+import DropMenu from "../../reused-Components/drop-down-menu/DropMenu";
+import DropContent from "../../reused-Components/drop-down-menu/drop-content/DropContent";
 import styles from "./Vehicles.module.css";
 import Navigation from "./navigation/Navigation";
 
@@ -6,6 +8,8 @@ const Vehicles = () => {
     <div className={`${styles.wrapper} ${styles.showWrapper}`}>
       <div className={`${styles.content} ${styles.showContent}`}>
         <Navigation />
+        <DropMenu />
+        <DropContent />
       </div>
     </div>
   );
---
+++ src/reused-Components/drop-down-menu/drop-content/DropContent.jsx
@@ -0,0 +1,7 @@
+import styles from "./DropContent.module.css";
+
+const DropContent = () => {
+  return <div className={styles.wrapper}></div>;
+};
+
+export default DropContent;
