ad3092d711f6cd2f021f4b4f07447a8b9e0e5d44
oleg <278381@gmail.com>
2024-05-18 18:22:09 +0300

    работа с footer

--- src/components/footer/footer-block-text/FooterBlockText.jsx
+++ src/components/footer/footer-block-text/FooterBlockText.jsx
@@ -1,7 +1,19 @@
 import styles from "./FooterBlockText.module.css";
+import logo from "./../../../img/components-img/logo-white.png";
 
 const FooterBlockText = () => {
-  return <div className={styles.wrapper}>FooterBlockText</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.blockImg}>
+        <img src={logo} alt="logo" />
+      </div>
+      <p className={styles.text1}>© JAGUAR LAND ROVER LIMITED 2024</p>
+      <p className={styles.text2}>
+        Registered office: Abbey Road, Whitley, Coventry CV3 4LF. <br />
+        Registered in England No: 1672070
+      </p>
+    </div>
+  );
 };
 
 export default FooterBlockText;
