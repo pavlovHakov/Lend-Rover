8d01c6a22922d353e29dc1e4650b3a4e9022f87a
oleg <278381@gmail.com>
2024-05-22 23:55:47 +0300

    card-transition

--- src/reused-Components/Card/Card.jsx
+++ src/reused-Components/Card/Card.jsx
@@ -4,7 +4,10 @@ import FollowingLink from "./../followingLink/FollowingLink";
 const Card = ({ img, title, subtitle }) => {
   return (
     <div className={styles.block}>
-      <img className={styles.img} src={img} alt="img" />
+      <div className={styles.imgBlock}>
+        <img className={styles.img} src={img} alt="img" />
+        <div className={styles.blackBlock}></div>
+      </div>
       <h4 className={styles.title}>{title}</h4>
       <p className={styles.subtitle}>{subtitle}</p>
       <FollowingLink text="explore" />
