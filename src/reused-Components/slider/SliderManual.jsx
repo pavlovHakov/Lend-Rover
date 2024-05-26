799c1f8c719cdfcb6e4ab076eecba94cce8e5773
oleg <278381@gmail.com>
2024-05-26 17:58:46 +0300

    переделка Slider-Manual

--- src/reused-Components/slider/SliderManual.jsx
+++ src/reused-Components/slider/SliderManual.jsx
@@ -1,46 +1,11 @@
-import React, { useRef } from "react";
-import Slider from "react-slick";
-import "./Slider.css";
-import "./SliderManual.css";
+import styles from "./SliderManual.module.css";
 
-function SliderManual({ img1, img2, img3 }) {
-  let sliderRef = useRef(null);
-
-  function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "flex", flexDirection: "column" }}
-        onClick={onClick}
-      />
-    );
-  }
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    swipeToSlide: true,
-    adaptiveHeight: true,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SampleNextArrow />,
-  };
+const SliderManual = ({ img1, img2, img3 }) => {
   return (
-    <div className="slider-container">
-      <Slider ref={(slider) => (sliderRef = slider)} {...settings}>
-        <div className="arrow-hover">
-          <img className="slider-img" src={img1} alt="img" />
-        </div>
-        <div className="arrow-hover">
-          <img className="slider-img" src={img2} alt="img" />
-        </div>
-        <div className="arrow-hover">
-          <img className="slider-img" src={img3} alt="img" />
-        </div>
-      </Slider>
+    <div className={styles.wrapper}>
+      <img className={styles.img} src={img1} alt="img" />
     </div>
   );
-}
+};
+
 export default SliderManual;
